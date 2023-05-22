374577a6d0c8d7a762c9a5b4a89452e892c2cec9
ondrej-vostarek <vostarek.ondrej@gmail.com>
2023-05-22 22:22:26 +0200

    structural data check - rest - fm update

--- 0.functions.R
+++ 0.functions.R
@@ -231,6 +231,9 @@ check_structural_data <- function(data, fk) {
   error.list$P_aspect <- data$plot %>% filter(!is.na(aspect) & !aspect %in% c(0:360))
   error.list$P_landform <- data$plot %>% filter(!landform %in% c(1:5, NA))
   error.list$P_hillform <- data$plot %>% filter(!hillform %in% c(1:3, NA))
+  error.list$P_duplicates <- as.tibble(duplicated(data$plot)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$plot %>% rownames_to_column("id"), by = "id")
+  error.list$P_ak <- data$plot %>% group_by(date, plotid) %>% filter(n() > 1)
   
   # tree
   
@@ -271,10 +274,13 @@ check_structural_data <- function(data, fk) {
   error.list$T_decayht_stump <- data$tree %>% filter(status %in% c(0, 10) & !decayht %in% 0)
   error.list$T_decayht_decay <- data$tree %>% filter(decay %in% 5 & !decayht %in% 0)
   error.list$T_decayht_height <- data$tree %>% filter(!status %in% c(1:4) & !is.na(height_m))
+  error.list$T_duplicates <- as.tibble(duplicated(data$tree)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$tree %>% rownames_to_column("id"), by = "id")
+  error.list$T_ak <- data$tree %>% group_by(date, plotid, treeid) %>% filter(n() > 1)
   
   # mortality
   
-  error.list$Mo_mort_agent <- data$mortality %>% filter(!mort_agent %in% fk$mort_agent_fk)
+  error.list$Mo_not_in_tree <- anti_join(data$mortality, data$tree, by = c("date", "treeid"))
   error.list$Mo_alive <- data$tree %>% filter(status %in% c(1:4)) %>% inner_join(., data$mortality, by = "treeid")
   error.list$Mo_dead <- data$tree %>% select(treeid, status_new = status) %>%
     inner_join(.,
@@ -282,31 +288,36 @@ check_structural_data <- function(data, fk) {
                by = "treeid") %>%
     filter(!status_old %in% c(1:4) & !status_new %in% c(1:4)) %>%
     inner_join(., data$mortality, by = "treeid")
+  error.list$Mo_mort_agent <- data$mortality %>% filter(!mort_agent %in% fk$mort_agent_fk)
   error.list$Mo_0_51 <- data$mortality %>% filter(mort_agent %in% c(0, 51)) %>%
     mutate(plotid = substr(treeid, 1, nchar(treeid) - 4)) %>%
     group_by(plotid, mort_agent) %>%
-    summarise(n = n()) %>%
-    filter(mort_agent %in% 0 & n > 2 |
-             mort_agent %in% 51 & n < 3)
+    filter((mort_agent %in% 0 & n() > 2) | (mort_agent %in% 51 & n() < 3))
+  error.list$Mo_duplicates <- as.tibble(duplicated(data$mortality)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$mortality %>% rownames_to_column("id"), by = "id")
+  error.list$Mo_ak <- data$mortality %>% group_by(date, treeid, mort_agent) %>% filter(n() > 1)
   
   # microsites
   
   error.list$Mi_not_in_tree <- anti_join(data$microsites, data$tree, by = c("date", "treeid"))
   error.list$Mi_microsite <- data$microsites %>% filter(!microsite %in% fk$microsite_fk)
-  error.list$Mi_count <- data$microsites %>% filter(microsite %in% c(11, 20, 23, 25, 26, 29, 32:41, 44:47) & count > 1)
+  error.list$Mi_count <- data$microsites %>% filter(is.na(count) | count < 1)
+  error.list$Mi_countable <- data$microsites %>% filter(microsite %in% c(11, 20, 23, 25, 26, 29, 32:41, 44:47) & count > 1)
   error.list$Mi_duplicates <- as.tibble(duplicated(data$microsites)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
     inner_join(., data$microsites %>% rownames_to_column("id"), by = "id")
+  error.list$Mi_ak <- data$microsites %>% group_by(date, treeid, microsite) %>% filter(n() > 1)
   
   # deadwood
   
   error.list$D_not_in_plot <- anti_join(data$deadwood, data$plot, by = c("date", "plotid"))
   error.list$D_transect <- data$deadwood %>% filter(!transect %in% fk$transect_fk)
   error.list$D_species <- data$deadwood %>% filter(!species %in% fk$species_fk)
-  error.list$D_dbh_mm <- data$deadwood %>% filter(dbh_mm %in% 0)
+  error.list$D_dbh_mm <- data$deadwood %>% filter(is.na(dbh_mm) | dbh_mm < 1)
   error.list$D_dbh_min <- data$deadwood %>% filter(dbh_mm < 60)
   error.list$D_decay <- data$deadwood %>% filter(!decay %in% fk$decay_wood_fk)
   error.list$D_duplicates <- as.tibble(duplicated(data$deadwood)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
     inner_join(., data$deadwood %>% rownames_to_column("id"), by = "id")
+  error.list$D_ak <- data$deadwood %>% group_by(date, plotid, transect, species, dbh_mm, decay) %>% filter(n() > 1)
   
   # regeneration
   
@@ -314,9 +325,10 @@ check_structural_data <- function(data, fk) {
   error.list$R_species <- data$regeneration %>% filter(!species %in% fk$species_fk)
   error.list$R_htclass <- data$regeneration %>% filter(!htclass %in% fk$htclass_fk)
   error.list$R_regeneratedon <- data$regeneration %>% filter(!regeneratedon %in% fk$regeneratedon_fk)
-  error.list$R_count <- data$regeneration %>% filter(count %in% 0)
+  error.list$R_count <- data$regeneration %>% filter(is.na(count) | count < 1)
   error.list$R_duplicates <- as.tibble(duplicated(data$regeneration)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
     inner_join(., data$regeneration %>% rownames_to_column("id"), by = "id")
+  error.list$R_ak <- data$regeneration %>% group_by(date, plotid, species, htclass, regeneratedon) %>% filter(n() > 1)
   
   # regeneration_subplot
   
@@ -327,9 +339,10 @@ check_structural_data <- function(data, fk) {
   error.list$RS_htclass <- data$regeneration_subplot %>% filter(!htclass %in% fk$htclass_fk)
   error.list$RS_browsing <- data$regeneration_subplot %>% filter(!browsing %in% fk$browsing_fk)
   error.list$RS_regeneratedon <- data$regeneration_subplot %>% filter(!regeneratedon %in% fk$regeneratedon_fk)
-  error.list$RS_count <- data$regeneration_subplot %>% filter(count %in% 0)
+  error.list$RS_count <- data$regeneration_subplot %>% filter(is.na(count) | count < 1)
   error.list$RS_duplicates <- as.tibble(duplicated(data$regeneration_subplot)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
     inner_join(., data$regeneration_subplot %>% rownames_to_column("id"), by = "id")
+  error.list$RS_ak <- data$regeneration_subplot %>% group_by(date, plotid, subplot_n, species, htclass, regeneratedon, browsing) %>% filter(n() > 1)
   
   # soil
   
@@ -339,30 +352,42 @@ check_structural_data <- function(data, fk) {
   error.list$S_bedrock <- data$soil %>% mutate(n = ifelse(soil_horizon %in% "R", 1, 0)) %>% 
     group_by(date, plotid, sample) %>% summarise(n = sum(n)) %>% filter(!n %in% 1)
   error.list$S_bedrock_depth <- data$soil %>% filter(soil_horizon %in% "R" & !depth_cm %in% c(-1, 0, 1))
-  error.list$S_depth_cm <- data$soil %>% filter(!soil_horizon %in% "R") %>% filter(depth_cm %in% c(NA, 0) | depth_cm < 0)
+  error.list$S_depth_cm <- data$soil %>% filter(!soil_horizon %in% "R") %>% filter(is.na(depth_cm) | depth_cm <= 0)
+  error.list$S_duplicates <- as.tibble(duplicated(data$soil)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$soil %>% rownames_to_column("id"), by = "id")
+  error.list$S_ak <- data$soil %>% group_by(date, plotid, sample, soil_horizon) %>% filter(n() > 1)
   
   # vegetation
   
   error.list$V_not_in_plot <- anti_join(data$vegetation, data$plot, by = c("date", "plotid"))
   error.list$V_sampling_date <- data$vegetation %>% filter(is.na(sampling_date))
+  error.list$V_sampling_date_date <- data$vegetation %>% mutate(sampling = as.numeric(substr(sampling_date, 1, 4))) %>% rowwise() %>% filter(!sampling %in% date)
   error.list$V_large_gap <- data$vegetation %>% filter(!large_gap %in% fk$large_gap_fk)
   error.list$V_vegetationht <- data$vegetation %>% filter(!vegetationht %in% fk$vegetationheight_fk)
-  error.list$V_biotope_quality <- data$vegetation %>% filter(!biotope_quality %in% fk$biotope_quality_fk)
-  error.list$V_biotope_trend <- data$vegetation %>% filter(!biotope_trend %in% fk$biotope_trend_fk)
-  error.list$V_vegetation_cover <- data$vegetation %>% 
+  error.list$V_vegetation_cover <- data$vegetation %>% select(-sampling_date) %>%
     mutate(cover = vaccinium_myrtillus_per + rubus_per + bryopsida_per + polypodiopsida_per + poaceae_per + ericaceae_per + other_per) %>%
     gather(., family, value, vaccinium_myrtillus_per, rubus_per, bryopsida_per, polypodiopsida_per, poaceae_per, ericaceae_per, other_per) %>%
     group_by(plotid, vegetation_cover, cover) %>%
     summarise(value = max(value)) %>%
     filter(vegetation_cover > cover | vegetation_cover < value)
+  error.list$V_biotope_quality <- data$vegetation %>% filter(!biotope_quality %in% fk$biotope_quality_fk)
+  error.list$V_biotope_trend <- data$vegetation %>% filter(!biotope_trend %in% fk$biotope_trend_fk)
+  error.list$V_large_herbivore_feces <- data$vegetation %>% filter(large_herbivore_feces < 0)
+  error.list$V_duplicates <- as.tibble(duplicated(data$vegetation)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$vegetation %>% rownames_to_column("id"), by = "id")
+  error.list$V_ak <- data$vegetation %>% group_by(date, plotid, sampling_date) %>% filter(n() > 1)
   
   # habitat
   
   error.list$H_not_in_plot <- anti_join(data$habitat, data$plot, by = c("date", "plotid"))
   error.list$H_sampling_date <- data$habitat %>% filter(is.na(sampling_date))
-  error.list$H_species <- data$habitat %>% filter(!animal_species %in% fk$animal_species_fk)
+  error.list$H_sampling_date_date <- data$habitat %>% mutate(sampling = as.numeric(substr(sampling_date, 1, 4))) %>% rowwise() %>% filter(!sampling %in% date)
+  error.list$H_animal_species <- data$habitat %>% filter(!animal_species %in% fk$animal_species_fk)
   error.list$H_gender <- data$habitat %>% filter(!gender %in% fk$gender_fk)
   error.list$H_habitat_sign_type <- data$habitat %>% filter(!habitat_sign_type %in% fk$habitat_sign_type_fk)
+  error.list$H_duplicates <- as.tibble(duplicated(data$habitat)) %>% rownames_to_column("id") %>% filter(value %in% T) %>% 
+    inner_join(., data$habitat %>% rownames_to_column("id"), by = "id")
+  error.list$H_ak <- data$habitat %>% group_by(date, plotid, sampling_date, animal_species, gender, habitat_sign_type) %>% filter(n() > 1)
   
   return(error.list)
   
