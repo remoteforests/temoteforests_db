d36b2e26d2929b48809e964f94bc345b85cf3f23
ondrej-vostarek <vostarek.ondrej@gmail.com>
2024-02-06 15:24:16 +0100

    adding mortality_date column

--- data/control/dendrochrono/dendrochrono_control.R
+++ data/control/dendrochrono/dendrochrono_control.R
@@ -1,11 +1,10 @@
-# setup -------------------------------------------------------------------
+# 0. setup ----------------------------------------------------------------
 
 library(pool);library(tidyverse)
 
 source("pw.R")
-source("0.functions.R")
 
-# 0. _fk ------------------------------------------------------------------
+source("data/control/dendrochrono/dendrochrono_control_fc.R")
 
 fk <- dbGetQuery(KELuser, "SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename LIKE '%fk'")
 
@@ -17,11 +16,11 @@ for (i in fk$tablename) {
   
 }
 
-# DENDROCHRONOLOGICAL DATA ------------------------------------------------
+# 1. DENDROCHRONOLOGICAL DATA ---------------------------------------------
 
 setwd("C:/Users/Ondrej_Vostarek/Downloads")
 
-# 1. reading --------------------------------------------------------------
+# 1. 1. reading -----------------------------------------------------------
 
 st <- unique(gsub("^(.*)[:.:](.*)[:.:](.*)$", "\\2", list.files(pattern = '.csv')))
 
@@ -44,7 +43,7 @@ tree.db <- tbl(KELuser, "tree") %>%
              by = "plot_id") %>%
   collect()
 
-# 2. cleaning -------------------------------------------------------------
+# 1. 2. cleaning ----------------------------------------------------------
 
 ## check data
 
@@ -57,14 +56,14 @@ data.clean <- list()
 data.clean$core <- data.raw$core %>% distinct(., .keep_all = T) %>% mutate(coretype = 1) # 2 - mortality cores
 data.clean$ring <- data.raw$ring %>% distinct(., .keep_all = T)
 
-# 3. exporting ------------------------------------------------------------
+# 1. 3. exporting ---------------------------------------------------------
 
 name <- paste(unique(data.clean$core$date), substr(first(data.clean$core$treeid), 1, 3), st, sep = "_")
 
 write.table(data.clean$core, paste(name, "core.csv", sep = "_"), sep = ",", row.names = F, na = "")
 write.table(data.clean$ring, paste(name, "ring.csv", sep = "_"), sep = ",", row.names = F, na = "")
 
-# 4. change species -------------------------------------------------------
+# 1. 4. change species ----------------------------------------------------
 
 change.df <- read.table("change_species.csv", sep = ",", header = T, stringsAsFactors = F) %>% 
   select(treeid, species) %>% 
@@ -74,7 +73,6 @@ change.df <- read.table("change_species.csv", sep = ",", header = T, stringsAsFa
 
 change.df %>% dbWriteTable(KELadmin, c('public','change'), value = ., row.names = FALSE, overwrite = TRUE)
 
-# disconnection -----------------------------------------------------------
+# ! close database connection ---------------------------------------------
 
-poolClose(KELadmin)
-poolClose(KELuser)
+poolClose(KELadmin);poolClose(KELuser)
